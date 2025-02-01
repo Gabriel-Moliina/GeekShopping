da42d729bbcf436549eee7a3e76b624464c25eba
Gabriel Molina <biel_molina9@hotmail.com>
2025-02-01 19:15:02 -0300

    Implementando identity server

---
+++ GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,54 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.
+        UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 5)))
+    );
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builderIdentity.AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
